aa693eede99379cdc7bf0449b7c7641d885c9cf3
everinArt <everinartdik@gmail.com>
2022-06-07 19:43:12 +0300

    Add tru gitignore

--- app.rb
+++ app.rb
@@ -5,6 +5,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 # require 'pony'
 
+configure do
+	@db = SQLite3::Database.new 'barbershop.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS 
+		"Users" 
+		(
+			"Id"	INTEGER,
+			"Name"	TEXT,
+			"Phone"	TEXT,
+			"Date"	TEXT,
+			"barber"	TEXT,
+			"Color"	TEXT,
+			PRIMARY KEY("Id" AUTOINCREMENT)
+	    )'
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
@@ -70,27 +85,5 @@ post '/contacts' do
 
 	f_con = File.open './public/contacts.txt', 'a'
 	f_con.write "Email: #{@mail}, Отзыв: #{@text}"
-
-	
-	# Pony.mail(
-	#   :name => params[:name],
-	#   :mail => params[:mail],
-	#   :body => params[:body],
-	#   :to => 'everinartdik@gmail.com',
-	#   :subject => params[:name] + " has contacted you",
-	#   :body => params[:message],
-	#   :port => '587',
-	#   :via => :smtp,
-	#   :via_options => { 
-	#     :address              => 'smtp.gmail.com', 
-	#     :port                 => '587', 
-	#     :enable_starttls_auto => true, 
-	#     :user_name            => 'lumbee', 
-	#     :password             => 'p@55w0rd', 
-	#     :authentication       => :plain, 
-	#     :domain               => 'localhost.localdomain'
-	#   })
-	# redirect '/success' 
-
 	erb :message 
 end
